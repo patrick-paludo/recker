17c40f4b8b0cd9bf1c41553ef03518d8b996ae56
patrick-paludo <patrick.paludo@gmail.com>
2022-04-06 22:05:42 -0300

    testes recorder.js

---
+++ public/recorder.js
@@ -0,0 +1,73 @@
+// Imports modules.
+const fs = require('fs'),
+  path = require('path');
+const AudioRecorder = require('node-audiorecorder');
+
+// Constants.
+const DIRECTORY = 'examples-recordings';
+
+// Create path to write recordings to.
+if (!fs.existsSync(DIRECTORY)) {
+  fs.mkdirSync(DIRECTORY);
+}
+
+// Initialize recorder and file stream.
+const audioRecorder = new AudioRecorder({
+  program: 'sox',
+  silence: 0
+}, console);
+
+// Log information on the following events.
+audioRecorder.on('error', function () {
+  console.warn('Recording error.');
+});
+audioRecorder.on('end', function () {
+  console.warn('Recording ended.');
+});
+
+
+
+
+// Create file path with random name.
+const fileName = path.join(
+  DIRECTORY,
+  "Arquivo_De_Audio.wav"
+);
+console.log('Writing new recording file at:', fileName);
+
+// Create write stream.
+iniciaGravacao();
+
+setTimeout(() => {
+    paraGravacao()
+    setTimeout(() => {
+        iniciaGravacao()
+        setTimeout(() => {
+            paraGravacao()
+        }, 5000);
+    }, 5000);
+}, 5000);
+
+
+
+
+
+
+
+function iniciaGravacao(){
+    const fileStream = fs.createWriteStream(fileName, { encoding: 'binary' });
+    audioRecorder.start().stream().pipe(fileStream);
+}
+
+function paraGravacao(){
+    audioRecorder.stop();
+}
+
+
+
+// Start and write to the file.
+
+
+// Keep process alive.
+process.stdin.resume();
+console.warn('Press ctrl+c to exit.');
